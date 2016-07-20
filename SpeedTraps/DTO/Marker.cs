60eabf00b97e6dfcfa389644620d64c7dc25aa97
Peter Vargovcik <peter.vargovcik@gmail.com>
2016-07-21 00:53:11 +0100

    SpeedTrapsInitial Commit

---
+++ SpeedTraps/DTO/Marker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedTraps.DTO
+{
+    public class Marker
+    {
+        int ID { get; set; }
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+        string Type { get; set; }
+        string County { get; set; }
+        string RoadName { get; set; }
+        string RoadNumber { get; set; }
+        int SpeedLimit { get; set; }
+        string Direction { get; set; }
+        string Details { get; set; }
+        DateTime Date { get; set; }
+    }
+
+    /*
+    <marker 
+        id = "147" 
+        lat="53.56249" 
+        lng="-6.44285" 
+        type="Static Camera (Gatso)" 
+        county="Meath" 
+        roadName="Ashbourne-Slane" 
+        roadNumber="N2" 
+        limit="100" 
+        direction="South" 
+        details="Fixed camera about 5km north of ashbourne. Between Kilmoon Cross (Junction with R153 Drogheda Rd) and Statoil garage Coolfore." 
+        date="3/30/2007" 
+        time="10:30" 
+    />
+    */
+}
---
+++ SpeedTraps/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using SpeedTraps.DTO;
+
+namespace SpeedTraps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://www.irishspeedtraps.com/images/636046524329307506.jpg";
+            StringBuilder output = new StringBuilder();
+
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(url);
+
+            XmlNodeList itemNodes = xmlDoc.SelectNodes("//markers/marker");
+
+            foreach (XmlNode itemNode in itemNodes)
+            {
+                DTO.Marker marker = _parseMarker(itemNode);
+                
+            }
+            /*
+            // Create an XmlReader
+            using (XmlReader reader = XmlReader.Create(url))
+            {
+                XmlWriterSettings ws = new XmlWriterSettings();
+                ws.Indent = true;
+                using (XmlWriter writer = XmlWriter.Create(output, ws))
+                {
+
+                    // Parse the file and display each of the nodes.
+                    while (reader.Read())
+                    {
+                        switch (reader.NodeType)
+                        {
+                            case XmlNodeType.Element:
+                                writer.WriteStartElement(reader.Name);
+                                break;
+                            case XmlNodeType.Text:
+                                writer.WriteString(reader.Value);
+                                break;
+                            case XmlNodeType.XmlDeclaration:
+                            case XmlNodeType.ProcessingInstruction:
+                                writer.WriteProcessingInstruction(reader.Name, reader.Value);
+                                break;
+                            case XmlNodeType.Comment:
+                                writer.WriteComment(reader.Value);
+                                break;
+                            case XmlNodeType.EndElement:
+                                writer.WriteFullEndElement();
+                                break;
+                        }
+                    }
+
+                }
+                
+            }
+            */
+
+        }
+
+        private static Marker _parseMarker(XmlNode itemNode)
+        {
+            
+
+            XmlNode titleNode = itemNode.SelectSingleNode("type");
+            XmlNode dateNode = itemNode.SelectSingleNode("roadName");
+            if ((titleNode != null) && (dateNode != null))
+                Console.WriteLine(dateNode.InnerText + ": " + titleNode.InnerText);
+
+            return new Marker();
+
+           // throw new NotImplementedException();
+        }
+    }
+}
